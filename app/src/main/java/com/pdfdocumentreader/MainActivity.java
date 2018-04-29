fee52da54343aaf16ceb1b38334efa2100f96f58
Mayur <prasadmayur.pm@gmail.com>
2018-04-29 18:22:49 +0530

    Initial Commit.

--- app/src/main/java/com/pdfdocumentreader/MainActivity.java
+++ app/src/main/java/com/pdfdocumentreader/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements PdfListAdapter.Ad
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
